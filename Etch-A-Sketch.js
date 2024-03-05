2d7187faee4ee49b58191019c9fb3042c01af9b9
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2024-03-05 10:10:42 -0500

    grid
    figuring out how to make the grid

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,7 +1,9 @@
 const container  = document.querySelector('.container')
-
+let subDivs;
 //create 16 divs
-for(let i = 0; i< 16; i++){
-    let subDivs = document.createElement('div')
+for(let i = 1; i< 16; i++){
+    subDivs = document.createElement('div')
+    subDivs.className = 'squares';
     container.appendChild(subDivs);
 }
+// subDivs.className = 'sqaures';
