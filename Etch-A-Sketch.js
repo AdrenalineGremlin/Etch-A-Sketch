6a25dbaa42344db5053fe70cfd98bdff19f4427f
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2024-03-05 19:01:32 -0500

    dont know what im doing
    tried to make the grid with flexbox

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,9 +1,21 @@
-const container  = document.querySelector('.container')
+const container  = document.querySelector('.container');
+const container2 = document.querySelector('.container2');
 let subDivs;
+let subDivs2;
+
 //create 16 divs
-for(let i = 1; i< 16; i++){
-    subDivs = document.createElement('div')
-    subDivs.className = 'squares';
+for(let rows =1; rows<=16; rows++){
+    subDivs = document.createElement('div');
+    subDivs.className = 'rows';
     container.appendChild(subDivs);
 }
+for(let cols =1; cols<=16; cols++){
+    subDivs2 = document.createElement('div');
+    subDivs2.className = 'cols';
+    container2.appendChild(subDivs2);
+
+}
+
+
+
 // subDivs.className = 'sqaures';
