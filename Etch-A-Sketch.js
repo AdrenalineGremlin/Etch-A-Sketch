9609aba8043b01a5615171b64871c9f7c8098690
l <96929613+AdrenalineGremlin@users.noreply.github.com>
2024-03-11 08:05:21 -0400

    figured out new grid
    pressing the button prompts user to enter a preffered grid

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,7 +1,32 @@
 const container = document.querySelector('.container');
 
-for(let i = 0; i < 256; i++){
-    const grid = document.createElement('div');
-    grid.className = 'grid-layout';
-    container.appendChild(grid)
+function createGrid(){
+    for(let i = 0; i < 256; i++){
+        const grid = document.createElement('div');
+        grid.className = 'grid-layout';
+        container.appendChild(grid)
+        grid.addEventListener('mouseover', ()=>{
+            grid.style.cssText = 'background-color: black;'
+        })
+    }
 }
+createGrid()
+const button  = document.querySelector('.btn');
+
+function askForGrid(grid){
+    button.addEventListener('click', ()=>{
+        const length = prompt("Enter a preffered  Length for grid: ");
+        const width = prompt("Enter a preffered  Width for grid: ");
+        grid.innerHTML = " ";
+        for(let i = 0; i<length*width;i++) {
+            const newGrid = document.createElement('div');
+            newGrid.className = 'new-grid-layout'
+            container.appendChild(newGrid);
+            newGrid.addEventListener('mouseover', ()=>{
+                newGrid.style.cssText = 'background-color: black;'
+            })
+        }
+    })
+   
+}
+askForGrid(container);
